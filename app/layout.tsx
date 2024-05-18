05260a142f2e72739032675db0ede62334695395
sachin.punyani <sachin.punyani@gmail.com>
2024-05-18 15:10:35 +0000

    chore: Add login page and configure Amplify client-side

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
+import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ConfigureAmplifyClientSide />
+
+        {children}
+        </body>
     </html>
   );
 }
---
+++ components/Login.tsx
@@ -0,0 +1,19 @@
+// components/Login.tsx
+"use client";
+import "@aws-amplify/ui-react/styles.css"
+
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { AuthUser } from "aws-amplify/auth";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+function Login({ user }: { user?: AuthUser }) {
+  useEffect(() => {
+    if (user) {
+      redirect("/");
+    }
+  }, [user]);
+  return null;
+}
+
+export default withAuthenticator(Login);
